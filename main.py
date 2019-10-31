401289f096d768c58f78dd8c91d623fc070cf02d
Josh Benson <josh.benson.805@gmail.com>
2019-10-31 10:18:50 -0700

    fixed switch added talon toggle

--- main.py
+++ main.py
@@ -1,46 +1,34 @@
 import os
+
 from kivy.app import App
-from threading import Thread
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix import slider
 from kivy.uix.screenmanager import ScreenManager, Screen
-
-from pidev.MixPanel import MixPanel
-from pidev.kivy.PassCodeScreen import PassCodeScreen
-from pidev.kivy.PauseScreen import PauseScreen
-from pidev.kivy import DPEAButton
-from pidev.kivy import ImageButton
 import spidev
+import os
+from threading import Thread
 from time import sleep
 import RPi.GPIO as GPIO
 from pidev.stepper import stepper
 from Slush.Devices import L6470Registers
 from pidev.Cyprus_Commands import Cyprus_Commands_RPi as cyprus
-
 spi = spidev.SpiDev()
-
-spi = spidev.SpiDev()
-
-spd = 400
-dir = 1
-pos = True
+from pidev.MixPanel import MixPanel
+from pidev.kivy.PassCodeScreen import PassCodeScreen
+from pidev.kivy.PauseScreen import PauseScreen
+from pidev.kivy import DPEAButton
+from pidev.kivy import ImageButton
 
 MIXPANEL_TOKEN = "x"
 MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
 
 SCREEN_MANAGER = ScreenManager()
 MAIN_SCREEN_NAME = 'main'
-IMAGE_SCREEN_NAME = 'image_screen'
 ADMIN_SCREEN_NAME = 'admin'
-s0 = stepper(port=0, micro_steps=32, hold_current=20, run_current=20, accel_current=20, deaccel_current=20,
-             steps_per_unit=200, speed=8)
-x = False
-speed = 10
-posCurrent = 0
-runningPreset = False
-runThread = False
+cyprus.initialize()
 
+
+#cyprus.setup_servo(1)
 
 class ProjectNameGUI(App):
     """
@@ -62,7 +50,54 @@ class MainScreen(Screen):
     """
     Class to handle the main screen and its associated touch events
     """
-
+    global pos
+    global servo
+    global talon
+    pos = False
+    servo = False
+    talon = False
+    global riseup
+    riseup = 1
+    def servoThread(self):
+        global servo
+        while servo:
+            if(cyprus.read_gpio() & 0b0001):
+                sleep(.1)
+                if (cyprus.read_gpio() & 0b0001):
+                    cyprus.set_servo_position(1, 0)
+            else:
+                cyprus.set_servo_position(1, 1)
+    def talonThread(self):
+        global talon
+        while talon:
+            if (cyprus.read_gpio() & 0b0001):
+                sleep(.1)
+                if (cyprus.read_gpio() & 0b0001):
+                    cyprus.set_pwm_values(1, period_value=100000, compare_value=50000,
+                                          compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+                    cyprus.set_servo_position(1, 1)
+            else:
+                cyprus.set_servo_position(1, .5)
+    def toggleServoSwitch(self):
+        global servo
+        if(servo == False):
+            self.ids.toggle_servo_switch.text = "Servo Switch On"
+            servo = True
+            Thread(target=self.servoThread).start()
+            Thread.daemon = True
+        else:
+            self.ids.toggle_servo_switch.text = "Servo Switch Off"
+            servo = False
+    def toggleTalonSwitch(self):
+        global talon
+        if(talon == False):
+            self.ids.toggle_talon_switch.text = "Talon Switch Off"
+            talon = True
+            Thread(target=self.talonThread).start()
+            Thread.daemon = True
+        else:
+            self.ids.toggle_talon_switch.text = "Talon Switch On"
+            talon = False
 
     def servoPressed(self):
         global pos
@@ -75,102 +110,27 @@ class MainScreen(Screen):
             cyprus.set_servo_position(1,1)
             pos = True
 
-    def servoSwitch(self):
-        global runThread
-        while runThread:
-            if (cyprus.read_gpio() & 0b0001) == 1:
-                sleep(.1)
-                if (cyprus.read_gpio() & 0b0001) == 1:
-                    cyprus.set_servo_position(1, 0)
-                    print("1")
-            else:
-                cyprus.set_servo_position(1, 1)
-                print("2")
-
-    def servoSwitchPressed(self, label):
-        toggle = label
-        global runThread
-        if toggle == "Enable Servo":
-            self.ids.servo_switch.text = "Disable Servo"
-            runThread = True
-            print("p1")
-        else:
-            self.ids.servo_switch.text = "Enable Servo"
-            runThread = False
-            print("p2")
-
-    def togglePressed(self, label):
-        toggle = label
-        global spd
-        global dir
-        if toggle == "On":
-            self.ids.toggle_text.text = "Off"
-            self.MotorOn()
-        else:
-            self.ids.toggle_text.text = "On"
-            s0.softStop()
-            s0.free_all()
-
-    def toggleDirPressed(self, label):
-        toggle = label
-        global spd
-        global dir
-        if toggle == "Clockwise":
-            self.ids.toggle_dir_text.text = "Counter Clockwise"
-            dir = 1
-        else:
-            self.ids.toggle_dir_text.text = "Clockwise"
-            dir = 0
-        self.MotorOn()
-
-    def MotorOn(self):
-        global dir
-        global spd
-        if self.ids.toggle_text.text == "Off":
-            spd = (self.ids.slider.value * 10)
-            s0.run(dir, spd)
-
-    def presetPressed(self):
-        global posCurrent
-        global runningPreset
-        s0.set_speed(1)
-        s0.start_relative_move(15)
-        while s0.is_busy():
-            posCurrent = s0.get_position_in_units()
-            self.ids.pos_label.text = "Position: " + str(posCurrent)
-            sleep(.01)
-        sleep(10)
-        s0.set_speed(5)
-        s0.start_relative_move(10)
-        while s0.is_busy():
-            posCurrent = s0.get_position_in_units()
-            self.ids.pos_label.text = "Position: " + str(posCurrent)
-            sleep(.01)
-        sleep(8)
-        s0.goHome()
-        while s0.is_busy():
-            posCurrent = s0.get_position_in_units()
-            self.ids.pos_label.text = "Position: " + str(posCurrent)
-            sleep(.01)
-        sleep(30)
-        s0.set_speed(8)
-        s0.start_relative_move(-100)
-        while s0.is_busy():
-            posCurrent = s0.get_position_in_units()
-            self.ids.pos_label.text = "Position: " + str(posCurrent)
-            sleep(.01)
-        sleep(10)
-        s0.goHome()
-        posCurrent = s0.get_position_in_units()
-        self.ids.pos_label.text = "Position: " + str(posCurrent)
-
     def pressed(self):
         """
         Function called on button touch event for button with id: testButton
         :return: None
         """
-        PauseScreen.pause(pause_scene_name='pauseScene', transition_back_scene='main', text="Test", pause_duration=5)
+        global riseup
+        if(riseup == 1):
+            cyprus.set_servo_position(1, 0)
+            riseup = 0
+            # 2 specifies port P5, i is a float that specifies speed
+        else:
+            cyprus.set_servo_position(1, 1)
+            riseup = 1
 
+
+    def cleanup(self):
+        cyprus.set_servo_position(1,.5)
+        cyprus.close()
+        spi.close()
+        GPIO.cleanup()
+        quit()
     def admin_action(self):
         """
         Hidden admin button touch event. Transitions to passCodeScreen.
@@ -193,10 +153,8 @@ class AdminScreen(Screen):
         """
         Builder.load_file('AdminScreen.kv')
 
-        PassCodeScreen.set_admin_events_screen(
-            ADMIN_SCREEN_NAME)  # Specify screen name to transition to after correct password
-        PassCodeScreen.set_transition_back_screen(
-            MAIN_SCREEN_NAME)  # set screen name to transition to if "Back to Game is pressed"
+        PassCodeScreen.set_admin_events_screen(ADMIN_SCREEN_NAME)  # Specify screen name to transition to after correct password
+        PassCodeScreen.set_transition_back_screen(MAIN_SCREEN_NAME)  # set screen name to transition to if "Back to Game is pressed"
 
         super(AdminScreen, self).__init__(**kwargs)
 
@@ -223,8 +181,6 @@ class AdminScreen(Screen):
         :return: None
         """
         quit()
-
-
 """
 Widget additions
 """
@@ -232,6 +188,7 @@ Widget additions
 Builder.load_file('main.kv')
 SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
 SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
+SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
 SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
 
 """
